76d430dc9d0ec86afb8159cb4a11ef9c8d45d9a3
Utkarsh <itsutkarsh123@gmail.com>
2022-08-25 23:15:02 +0530

    add theme for the project to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		fontFamily: {
+			"space-mono": ['Space Mono', 'monospace']
+		},
+		colors: {
+			"primary": "#26c0ab",
+			"secondary": "#00494d",
+			"gray-cyan": {
+				50: "#f4fafa",
+				100: "#c5e4e7",
+				500: "#7f9c9f",
+				800: "#5e7a7d"
+			}
+		},
 		extend: {},
 	},
 	plugins: [],
