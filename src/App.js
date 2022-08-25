12f241bfd19528969568af59174a6d04b48b58ee
Utkarsh <itsutkarsh123@gmail.com>
2022-08-26 01:44:52 +0530

    add basic design for number of tip-amount section

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 					TTER
 				</h1>
 			</div>
-			<div className="h-full flex flex-col gap-6 bg-white rounded-t-3xl p-8">
+			<div className="h-fit flex flex-col gap-6 bg-white rounded-t-3xl p-8">
 				<div>
 					<label
 						htmlFor="bill-amount"
@@ -80,6 +80,31 @@ const App = () => {
 							className=" text-secondary bg-gray-cyan-50 m-1 py-1 px-3 rounded-md text-right focus:rounded-md focus:outline-2 focus:outline-primary"
 						/>
 					</div>
+				</div>
+				<div className="grid grid-rows-3 bg-secondary rounded-xl p-6 gap-6">
+					<div className="flex justify-between">
+						<div className="">
+							<p className="text-sm text-gray-cyan-50 ">
+								Tip amount
+							</p>
+							<p className="text-xs text-gray-cyan-500">
+								/ person
+							</p>
+						</div>
+						<p className="text-primary ">$4.27</p>
+					</div>
+					<div className="flex justify-between">
+						<div className="">
+							<p className="text-sm text-gray-cyan-50 ">Total</p>
+							<p className="text-xs text-gray-cyan-500">
+								/ person
+							</p>
+						</div>
+						<p className="text-primary ">$32.79</p>
+					</div>
+					<button className="bg-primary text-lg text-secondary rounded-md">
+						RESET
+					</button>
 				</div>
 			</div>
 		</div>
