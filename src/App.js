fc07f1d6f400871a4ce488f11bd7d29bfe236dd3
Utkarsh <itsutkarsh123@gmail.com>
2022-08-26 01:21:08 +0530

    add basic design for select tip section

--- src/App.js
+++ src/App.js
@@ -28,13 +28,37 @@ const App = () => {
 				</div>
 				<div>
 					<p className="text-base text-gray-cyan-800">Select tip %</p>
-					<div className="grid grid-cols-2 grid-rows-3 gap-4">
-						<input className="bg-secondary text-gray-cyan-100 rounded-md py-1 " value="5%" type="button" />
-						<input className="bg-secondary text-gray-cyan-100 rounded-md py-1 " value="5%" type="button" />
-						<input className="bg-secondary text-gray-cyan-100 rounded-md py-1 " value="5%" type="button" />
-						<input className="bg-secondary text-gray-cyan-100 rounded-md py-1 " value="5%" type="button" />
-						<input className="bg-secondary text-gray-cyan-100 rounded-md py-1 " value="5%" type="button" />
-						<input className="rounded-md py-1 " placeholder="Custom" type="text" />
+					<div className="grid grid-cols-2 grid-rows-3 gap-4 my-2">
+						<input
+							className="bg-secondary active:bg-primary active:text-gray-cyan-800 text-gray-cyan-100 rounded-md py-1 "
+							value="5%"
+							type="button"
+						/>
+						<input
+							className="bg-secondary active:bg-primary active:text-gray-cyan-800 text-gray-cyan-100 rounded-md py-1 "
+							value="10%"
+							type="button"
+						/>
+						<input
+							className="bg-secondary active:bg-primary active:text-gray-cyan-800 text-gray-cyan-100 rounded-md py-1 "
+							value="15%"
+							type="button"
+						/>
+						<input
+							className="bg-secondary active:bg-primary active:text-gray-cyan-800 text-gray-cyan-100 rounded-md py-1 "
+							value="25%"
+							type="button"
+						/>
+						<input
+							className="bg-secondary active:bg-primary active:text-gray-cyan-800 text-gray-cyan-100 rounded-md py-1 "
+							value="50%"
+							type="button"
+						/>
+						<input
+							className="bg-gray-cyan-50 text-gray-cyan-800 focus:outline-2 focus:outline-primary rounded-md py-1 text-center"
+							placeholder="Custom"
+							type="number"
+						/>
 					</div>
 				</div>
 			</div>
