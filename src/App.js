31076fa758b0456637693c336e25a70358d8422a
Utkarsh <itsutkarsh123@gmail.com>
2022-08-28 21:45:05 +0530

    design modification for mobile

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TipPercent from "./components/TipPercent";
 
 const App = () => {
 	return (
-		<div className="bg-gray-cyan-100 text-2xl h-full w-screen font-space-mono">
+		<div className="bg-gray-cyan-100 text-2xl h-screen w-screen font-space-mono">
 			<div className="h-[15%] py-14  flex flex-col justify-center text-center w-fit mx-auto ">
 				<h1 className="tracking-[10px] text-gray-cyan-800">
 					SPLI
@@ -13,8 +13,8 @@ const App = () => {
 					TTER
 				</h1>
 			</div>
-			<div className="h-full justify-center gap-5 flex flex-col bg-white rounded-t-3xl p-8">
-				<div className="flex flex-col gap-5">
+			<div className="h-full gap-5 grid grid-rows-3 bg-white rounded-t-3xl p-8 ">
+				<div className="row-span-2 justify-evenly flex flex-col gap-5">
 					<BillAmount />
 					<TipPercent />
 					<NumberOfPeople />
