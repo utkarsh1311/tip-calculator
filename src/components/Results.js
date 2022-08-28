18d3c374763a7ac946d8803ec4d59d663e53876b
Utkarsh <itsutkarsh123@gmail.com>
2022-08-28 22:50:46 +0530

    add media-queries for result container

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,21 +1,21 @@
 const Results = () => {
 	return (
-		<div className="grid grid-rows-3 bg-secondary rounded-xl p-6 gap-6">
+		<div className="grid grid-rows-3 bg-secondary rounded-xl p-6 gap-6 lg:grid-rows-4 lg:p-8">
 			<div className="flex justify-between">
 				<div className="">
 					<p className="text-sm text-gray-cyan-50 ">Tip amount</p>
 					<p className="text-xs text-gray-cyan-500">/ person</p>
 				</div>
-				<p className="text-primary ">$4.27</p>
+				<p className="text-primary lg:text-4xl">$4.27</p>
 			</div>
 			<div className="flex justify-between">
 				<div className="">
 					<p className="text-sm text-gray-cyan-50 ">Total</p>
 					<p className="text-xs text-gray-cyan-500">/ person</p>
 				</div>
-				<p className="text-primary ">$32.79</p>
+				<p className="text-primary lg:text-4xl">$32.79</p>
 			</div>
-			<button className="bg-primary text-lg text-secondary rounded-md">
+			<button className="bg-primary text-lg text-secondary rounded-md lg:row-start-4">
 				RESET
 			</button>
 		</div>
